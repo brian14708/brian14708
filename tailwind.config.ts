c9fe3cba36a78197abf43f6d0bfef6b6396eda4a
Brian Li <me@brian14708.dev>
2023-03-29 08:20:44 +0800

    Use typescript config for tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,8 @@
+import type { Config } from "tailwindcss";
 import Typography from "@tailwindcss/typography";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   theme: {},
   plugins: [Typography],
-};
+} satisfies Config;
