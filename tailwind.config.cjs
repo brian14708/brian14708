571329513f2c3ddc4967ab102679d174f36479f1
Brian Li <me@brian14708.dev>
2023-03-29 08:10:33 +0800

    Migrate tailwind config to esm

--- tailwind.config.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
-  theme: {},
-  plugins: [require("@tailwindcss/typography")],
-};
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+import Typography from "@tailwindcss/typography";
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  theme: {},
+  plugins: [Typography],
+};
