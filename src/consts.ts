b2fd572747e2aee8133b3f262ed6d8320741977f
Brian Li <me@brian14708.dev>
2023-08-29 21:04:55 +0800

    Add RSS

--- src/consts.ts
+++ src/consts.ts
@@ -1,1 +1,2 @@
 export const SITE_TITLE = "Brian Li (@brian14708)";
+export const SITE_DESCRIPTION = "Brian's personal website";
---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,19 @@
+import rss from "@astrojs/rss";
+import type { APIContext } from "astro";
+import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
+import { getCollection } from "astro:content";
+
+export async function get(context: APIContext) {
+  const posts = await getCollection("posts");
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site || "",
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.date,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
+  });
+}
